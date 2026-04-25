74e47790f5247dc206b96bddb9a38da48ec748d6
mrsajid <syedsajeed6969@gmail.com>
2026-04-25 12:43:36 +0530

    greetings added

--- src/views/TodoPage.tsx
+++ src/views/TodoPage.tsx
@@ -10,14 +10,27 @@ const TodoPage = () => {
     const handleSubmit = (text: string) => {
         setTasks([...tasks, text])
     }
+    const getGreeting = ():string => {
+  const hour = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+    hour: "numeric",
+    hour12: false,
+  });
+
+  const h = parseInt(hour);
+
+  if (h < 12) return "Good Morning";
+  if (h < 17) return "Good Afternoon";
+  return "Good Evening";
+};
     return (
         <>
             <Navbar />
             <div className="todo-container">
                 <div className="todo-wrapper">
                     <div className="todo-header">
-                        <h1>✓ Taskly</h1>
-                        <p>Organize your day, one task at a time</p>
+                      <h1>{getGreeting()}</h1>
+<p>Turn your plans into progress — one task at a time.</p>
                     </div>
                     <Form onSubmit={handleSubmit} />
                     <ToDoList tasks={tasks} />
